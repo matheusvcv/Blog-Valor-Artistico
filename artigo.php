d2eea3924b9c0e18820a7119235d56e144ec4269
Matheus Vinicius <matheusvcv@yahoo.com.br>
2022-05-01 09:50:57 -0300

    Adicionado um arquivo css exclusivo para a página de leitura dos posts individualmente. Adicionado quebra de linha da exibição dos parágrafos. Pequenas adaptações feitas no arquivo index.php

--- artigo.php
+++ artigo.php
@@ -4,22 +4,22 @@
 	require"src/artigos.php";
 
 	$obj_artigo = new Artigos($conexao);
-
 	$artigo = $obj_artigo-> exibirPorId($_GET['id']);
 ?>
+
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width initial-scale=1.0">
 	<link rel="shortcut icon" href="img/cosvi.png">
+	<link rel="stylesheet" type="text/css" href="style1.css">
 	<title>Valor Artístico</title>
 </head>
 <body>
+	<div id="container">
 
-	<div align="center">
-
-		<h1><?php echo $artigo['titulo']; ?></h1>
+		<h1><?php echo nl2br($artigo['titulo']); ?></h1>
 
 		<p><?php echo $artigo['conteudo'] ?></p>
 
@@ -27,8 +27,5 @@
 			<a class="botao botao-block" href="index.php">Voltar</a>
 		</div>
 	</div>
-
-
-
 </body>
 </html>
--- index.php
+++ index.php
@@ -1,6 +1,6 @@
-/*Algumas tags obsoletas de html estão sendo utilizadas por enquanto, até que seja aplicado CSS*/
-
 <?php
+
+//Algumas tags obsoletas de html estão sendo utilizadas por enquanto, até que seja aplicado CSS
 
 	require "conex.php";
 	require "src/artigos.php";
@@ -14,55 +14,63 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width initial-scale=1.0">
 	<link rel="shortcut icon" href="img/cosvi.png" type="image/x-icon">
+	<link rel="stylesheet" href="style.css">
 	<title>Valor Artístico</title>
 </head>
 <body>
 	
-	<table width="98%"> 
-			<th><a href="" rel="">VA PodCast</a></th>
-			<th><a href="" rel="">Valor Da Vida</a></th>
-			<th><a href="" rel="">Experiências em Texto</th>
-			<th><a href="" rel="">Conheça Nossos Projetos</a></th>
-			<th><a href="" rel="">CostaViana Enterprise</th>
-		</tr>
-	</table><br>
+	<div id="container">
 
-	<div align="center">
+		<table width="100%"> 
+			<tr>
+				<th><a href="" rel="" style="color:white;">VA PodCast</a></th>
+				<th><a href="" rel="" style="color:white;">Valor Da Vida</a></th>
+				<th><a href="" rel="" style="color:white;">Experiências em Texto</a></th>
+				<th><a href="" rel="" style="color:white;">Conheça Nossos Projetos</a></th>
+				<th><a href="" rel="" style="color:white;">CostaViana Enterprise</a></th>
+			</tr>
+		</table><br>
 
-		<picture>
+		<div id="logo">
+			<picture id="logo">
 
-			<source media="(max-width: 225px)" srcset="img/logo2.png" type="image/png">
+				<source media="(max-width: 225px)" srcset="img/logo2.png" type="image/png">
 
-			<source media="(max-width: 300px)" srcset="img/logo1.png" type="image/png">
+				<source media="(max-width: 300px)" srcset="img/logo1.png" type="image/png">
 
-			<img src="img/logo.png" alt="Imagem Grande">
-		</picture>
-
+				<img src="img/logob.png" alt="Imagem Grande">
+			</picture>
+		</div>
 	</div>
 
-	<div align="left">
+		<div>
 
-		<h1>Sobre Nós:</h1>
+			<h1 id="sn">Sobre Nós:</h1>
 
-		<p>O Valor Artístico é um blog que tem o intuito de trazer ao público conteúdos voltados à sensação que sentimos quando algo nos toca profundamente. Nosso intuito é compartilhar essas experiências, de modo à conectar uma pluralidade de experiências individuais e aprender uns com os outros. Acreditamos que o compartilhamento de sentimentos ajuda na compreensão humana e na construção de relacionamentos.</p>
+			<p>O Valor Artístico é um blog que tem o intuito de trazer ao público conteúdos voltados à sensação que sentimos quando algo nos toca profundamente. Nosso intuito é compartilhar essas experiências, de modo à conectar uma pluralidade de experiências individuais e aprender uns com os outros. Acreditamos que o compartilhamento de sentimentos ajuda na compreensão humana e na construção de relacionamentos.</p>
 
-		<p><strong>Atenção:</strong> Entendemos que cada indivíduo é dententor de uma sensibilidade artística única, que tem a ver com sua própria inividualidade e essência. Sendo assim, a arte fala com cada um nós de maneira específica. Não acreditamos na utilização de padrões técncos para avaliar expressões artísticas, e nem na hierarquização de obras ou experiências. O Valor Artístico tem a ver com perspectivas e é qualitativo, não quantitativo.</p>
-		
-	</div>
+			<p><strong>Atenção:</strong> Entendemos que cada indivíduo é dententor de uma sensibilidade artística única, que tem a ver com sua própria inividualidade e essência. Sendo assim, a arte fala com cada um nós de maneira específica. Não acreditamos na utilização de padrões técncos para avaliar expressões artísticas, e nem na hierarquização de obras ou experiências. O Valor Artístico tem a ver com perspectivas e é qualitativo, não quantitativo.</p>
+			
+		</div>
 
-	<div align="center">
-		<h1>Experiências Escritas</h1>
-	</div>
+		<div id="texto">
+			<h1>Experiências Escritas</h1>
+		</div>
 
+			<div id="artigo">
 
-		<?php foreach($artigos as $artigo): ?>
+				<?php foreach($artigos as $artigo): ?>
 
-		<h2><?php echo $artigo['titulo']; ?></h2>
+				<h2><?php echo $artigo['titulo']; ?></h2>
 
-		<p><?php echo $artigo['resumo']; ?></p>
+				<p><?php echo $artigo['resumo']; ?></p><br>
+				<p><a href="artigo.php?id=<?php echo $artigo['id']; ?>">Leia Mais</a></p>
 
-		<?php endforeach; ?>
+				<?php endforeach; ?>
 
+			</div>
+
+	</div><br>
 
 
 	<div align="center">
