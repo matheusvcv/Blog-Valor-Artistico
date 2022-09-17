01839fd0458dae6751dce90c370e7b27c99260f1
Matheus Vinicius <matheusvcv@yahoo.com.br>
2022-09-16 21:18:49 -0300

    Adicionado o bootstrap via CDN

--- index.php
+++ index.php
@@ -14,6 +14,7 @@
 	<meta name="viewport" content="width=device-width initial-scale=1.0">
 	<link rel="shortcut icon" href="img/cosvi.png" type="image/x-icon">
 	<link rel="stylesheet" type="text/css" href="style.css">
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
 	<title>Valor Artístico</title>
 </head>
 <body>
